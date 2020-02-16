e6cd116faeffda001ecbab9e813d581404662229
Ray Donnelly <mingw.android@gmail.com>
2020-02-16 18:49:59 +0000

    Proper debug build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,7 @@
 setlocal EnableDelayedExpansion
 
+echo on
+
 :: brand Python with conda-forge startup message
 ::%SYS_PYTHON% %RECIPE_DIR%\brand_python.py
 ::if errorlevel 1 exit 1
@@ -21,26 +23,61 @@ for /f "usebackq delims=" %%i in (`conda list -p %PREFIX% sqlite --no-show-chann
 for /f "tokens=2 delims==/ " %%i IN ('echo %SQLITE3_VERSION_LINE%') do (set SQLITE3_VERSION=%%~i)
 echo SQLITE3_VERSION detected as %SQLITE3_VERSION%
 
-cd PCbuild
-if "%DEBUG_C%"=="yes" (
+if "%PY_INTERP_DEBUG%" neq "" (
+  set CONFIG=-d
+  set _D=_d
+) else (
+  set CONFIG=
+  set _D=
+)
+
+
+if "%PY_INTERP_DEBUG%"=="yes" (
   set PGO=
 ) else (
-  set PGO=--pgo
+  if "%DEBUG_C%"=="yes" (
+    set PGO=
+  ) else (
+    set PGO=--pgo
+    set PGO=
+  )
 )
-set PGO=
 
-call build.bat %PGO% -m -e -v -p %PLATFORM%
+cd PCbuild
+
+:: Twice because I am changing zipimport ATM.
+call build.bat %PGO% %CONFIG% -m -e -v -p %PLATFORM%
+call build.bat %PGO% %CONFIG% -m -e -v -p %PLATFORM%
 if errorlevel 1 exit 1
 cd ..
 
 :: Populate the root package directory
-for %%x in (python37.dll python3.dll python.exe pythonw.exe venvlauncher.exe venvwlauncher.exe) do (
+for %%x in (python37%_D%.dll python3%_D%.dll python%_D%.exe pythonw%_D%.exe venvlauncher%_D%.exe venvwlauncher%_D%.exe) do (
+    echo Copying: %SRC_DIR%\PCbuild\%BUILD_PATH%\%%x to %PREFIX%
     copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\%%x %PREFIX%
     if errorlevel 1 exit 1
 )
 
-for %%x in (python.pdb python37.pdb pythonw.pdb) do (
+:: If _d appears anywhere other than at the end of the filename then this will break.
+if "%_D%"=="_d" (
+  for %%x in (python37%_D%.dll python3%_D%.dll python%_D%.exe pythonw%_D%.exe venvlauncher%_D%.exe venvwlauncher%_D%.exe) do (
+    set _TMP=%%x
+    call set _DST=%%_TMP:_D=%%
+    echo Copying: %SRC_DIR%\PCbuild\%BUILD_PATH%\%%x to !_DST!
+    copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\%%x %PREFIX%\!_DST!
+    if errorlevel 1 exit 1
+  )
+)
+
+for %%x in (python%_D%.pdb python37%_D%.pdb pythonw%_D%.pdb) do (
+    echo Copying: %SRC_DIR%\PCbuild\%BUILD_PATH%\%%x %PREFIX%
     copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\%%x %PREFIX%
+    if errorlevel 1 exit 1
+)
+
+for %%x in (*.pdb) do (
+    echo Copying PDB: %SRC_DIR%\PCbuild\%BUILD_PATH%\%%x to %PREFIX%\DLLs
+    copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\%%x %PREFIX%\DLLs
     if errorlevel 1 exit 1
 )
 
@@ -121,13 +158,20 @@ if errorlevel 1 exit 1
 
 :: Populate the libs directory
 mkdir %PREFIX%\libs
-copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\python37.lib %PREFIX%\libs\
+copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\python37%_D%.lib %PREFIX%\libs\
 if errorlevel 1 exit 1
-copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\python3.lib %PREFIX%\libs\
+copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\python3%_D%.lib %PREFIX%\libs\
 if errorlevel 1 exit 1
-copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\_tkinter.lib %PREFIX%\libs\
+copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\_tkinter%_D%.lib %PREFIX%\libs\
 if errorlevel 1 exit 1
-
+if "%_D%"=="_d" (
+  copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\python37%_D%.lib %PREFIX%\libs\python37.lib
+  if errorlevel 1 exit 1
+  copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\python3%_D%.lib %PREFIX%\libs\python3.lib
+  if errorlevel 1 exit 1
+  copy /Y %SRC_DIR%\PCbuild\%BUILD_PATH%\_tkinter%_D%.lib %PREFIX%\libs\_tkinter.lib
+  if errorlevel 1 exit 1
+)
 
 :: Populate the Lib directory
 del %PREFIX%\libs\libpython*.a
@@ -154,11 +198,21 @@ if errorlevel 1 exit 1
 rd /s /q %PREFIX%\Lib\lib2to3\tests\
 if errorlevel 1 exit 1
 
+:: We need our Python to be found
+if "%_D%" neq "" copy %PREFIX%\python%_D%.exe %PREFIX%\python.exe
+if "%_D%" neq "" copy %PREFIX%\python%_D%.pdb %PREFIX%\python.pdb
+
 %PREFIX%\python.exe -Wi %PREFIX%\Lib\compileall.py -f -q -x "bad_coding|badsyntax|py2_" %PREFIX%\Lib
 if errorlevel 1 exit 1
 
 :: Pickle lib2to3 Grammar
 %PREFIX%\python.exe -m lib2to3 --help
+
+echo CONDA_EXE is %CONDA_EXE%
+echo where conda is
+where conda
+echo where python is
+where python
 
 echo "Testing print() does not print Hello"
 conda run -p %PREFIX% python -c "print()" 2>&1 | findstr /r /c:"Hello"
