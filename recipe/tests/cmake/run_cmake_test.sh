470e7d8c6d9723006bbc764d62f55843142512b9
Ray Donnelly <mingw.android@gmail.com>
2020-10-13 04:40:11 +0200

    Some test fixes

--- recipe/tests/cmake/run_cmake_test.sh
+++ recipe/tests/cmake/run_cmake_test.sh
@@ -4,5 +4,5 @@ set +e
 
 CMAKE_DBG="trace --debug-find --debug-output --debug-trycompile"
 CMAKE_DBG=--debug-find
-cmake -G"GNU Makefiles" -DPY_VER=${1} ${CMAKE_DBG} .
+cmake -G"Unix Makefiles" -DPY_VER=${1} ${CMAKE_DBG} .
 make
