db9b41ad1958d32e6ede5fcd621b0f82419c130c
Jaida Rice <jaidarice@gmail.com>
2023-02-27 12:50:45 -0800

    Fix branding

--- recipe/brand_python.py
+++ recipe/brand_python.py
@@ -35,7 +35,7 @@ def patch_get_version(msg):
 
 msg = os.environ.get('python_branding', '<undefined>')
 if msg == '<undefined>':
-    msg = "| packaged by conda-forge |" 
+    msg = "| packaged by Anaconda, Inc. |" 
 
 patch_platform(msg)
 patch_get_version(msg)
